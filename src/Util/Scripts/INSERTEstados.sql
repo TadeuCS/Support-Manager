9bfd6f8122c09c56d22ead05cc65a0c5f778d4b5
Fabyana Diaz <Fabyana Diaz@PC.Home>
2014-08-16 16:21:13 -0300

    Criação Insert Cidade e Estado

---
+++ src/Util/Scripts/INSERTEstados.sql
@@ -0,0 +1,29 @@
+INSERT INTO uf(codUF, sigla, descricao) VALUES (11,'RO','RONDÔNIA');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (12,'AC','ACRE');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (13,'AM','AMAZONAS');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (14,'RR','RORAIMA');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (15,'PA','PARÁ');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (16,'AP','AMAPÁ');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (17,'TO','TOCANTINS');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (21,'MA','MARANHÃO');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (22,'PI','PIAUÍ');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (23,'CE','CEARÁ');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (24,'RN','RIO GRANDE DO NORTE');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (25,'PB','PARAÍBA');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (26,'PE','PERNAMBUCO');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (27,'AL','ALAGOAS');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (28,'SE','SERGIPE');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (29,'BA','BAHIA');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (31,'MG','MINAS GERAIS');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (32,'ES','ESPIRITO SANTO');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (33,'RJ','RIO DE JANEIRO');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (35,'SP','SÃO PAULO');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (41,'PR','PARANÁ');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (42,'SC','SANTA CATARINA');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (43,'RS','RIO GRANDE DO SUL');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (50,'MS','MATO GROSSO DO SUL');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (51,'MT','MATO GROSSO');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (52,'GO','GOIÁS');
+INSERT INTO uf(codUF, sigla, descricao) VALUES (53,'DF','DISTRITO FEDERAL');
+
+
