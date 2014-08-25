83c14bbcbbbd11d6aaccba0c50d5f7a5fa4705b0
Tadeu <Tadeu@Tadeu-NB.Home>
2014-08-24 23:29:14 -0300

    novas bibliotecas, e classes para relatorios...

---
+++ src/Util/Classes/GeraRelatorios.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util.Classes;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
+/**
+ *
+ * @author Tadeu
+ */
+public class GeraRelatorios {
+
+    public String getDiretorio(String nomeArquivo) {
+        File arquivo = new File(nomeArquivo);
+        return arquivo.getAbsolutePath();
+    }
+
+    public void imprimirRelatorioEmCodigo() {
+        Connection conn = null;
+        try {
+            // Obtém o diretório da aplicação
+
+            // Carrega conexão via JDBC
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:1527/banco", "usuario", "senha");
+            Statement sql = conn.createStatement();
+
+            // Carrega fonte de dados
+            ResultSet rs = sql.executeQuery("SELECT * from sua tabela");
+            JRDataSource ds = new JRResultSetDataSource(rs);
+
+            // Preenche o relatório com os dados
+            JasperPrint print = JasperFillManager.fillReport(getDiretorio("Teste") + "/arquivo.jasper", null, ds);
+
+            // Exibe visualização dos dados
+            JasperViewer.viewReport(print, false);
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (JRException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+    }
+
+    public void imprimirRelatorioSQLNoRelatorio() {
+        Connection conn = null;
+        try {
+            // Obtém o diretório da aplicação
+
+            // Carrega conexão via JDBC
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:1527/banco", "usuario", "senha");
+
+            Map parameters = new HashMap();
+//            parameters.put("codigo", jtCodigo.getText());
+            parameters.put("codigo", 1);
+
+            // Preenche o relatório com os dados
+            JasperPrint print = JasperFillManager.fillReport(getDiretorio("teste") + "/arquivo.jasper", parameters, conn);
+
+            // Exibe visualização dos dados
+            JasperViewer.viewReport(print, false);
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        } catch (JRException ex) {
+            System.out.println(ex);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex);
+            }
+        }
+
+    }
+}
---
+++ src/Util/Classes/PropertiesManager.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util.Classes;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import javax.swing.JOptionPane;
+
+public class PropertiesManager {
+    
+    //altere de acordo com o nome do arquivo properties criado
+    static String file = "dados.properties";
+    FileOutputStream fos;
+    FileInputStream fis;
+    Properties properties = new Properties();
+
+    public PropertiesManager() {
+        cria();
+    }
+
+    private String getCaminho() {
+        File file = new File(this.file);
+        //altere de acordo com o diretorio do seu arquivo properties
+        return file.getAbsolutePath().replaceAll(this.file, "").concat("src\\Util\\Classes\\");
+    }
+
+    private void cria() {
+        try {
+            //Criamos um objeto FileOutputStream
+            //Setamos o arquivo que será lido
+            fis = new FileInputStream(getCaminho() + file);
+            properties.load(fis);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+    }
+
+    public void altera(String campo, String valor) {
+        try {
+            fos = new FileOutputStream(getCaminho() + file);
+            //altera do campo
+            properties.setProperty(campo, valor);
+            //grava os dados no arquivo
+            properties.store(fos, campo);
+            fos.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    public String ler(String campo) {
+        try {
+            //método load faz a leitura através do objeto fis
+            properties.load(fis);
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        //Captura o valor da propriedade, através do nome da propriedade(Key)
+        return properties.getProperty(campo);
+    }
+}
