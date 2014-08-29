97cad81bed3f12b63505f8317ee14f18a618fbbd
Tadeu <Tadeu@Tadeu-PC>
2014-08-29 17:35:52 -0300

    FRAME frames de consulta

---
+++ src/View/Atendimento/Frm_ConAtendimento_Pendentes.java
@@ -0,0 +1,275 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package View.Atendimento;
+
+import Controller.ClienteDAO;
+import View.Cadastros.Frm_CadUsuario;
+import javax.swing.JOptionPane;
+import javax.swing.RowFilter;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
+
+/**
+ *
+ * @author Tadeu
+ */
+public class Frm_ConAtendimento_Pendentes extends javax.swing.JFrame {
+
+    DefaultTableModel model;
+    ClienteDAO clienteDAO;
+    public Frm_ConAtendimento_Pendentes() {
+        initComponents();
+        model = (DefaultTableModel) tb_clientes.getModel();
+        setVisible(true);
+        listaClientes();
+    }
+
+        public void listaClientes() {
+        clienteDAO = new ClienteDAO();
+        try {
+            int i = 0;
+            limpaTabela();
+            while (i < clienteDAO.lista().size()) {
+                String[] linha = new String[]{
+                    clienteDAO.lista().get(i).getCodcliente().toString(),
+                    clienteDAO.lista().get(i).getReferencia().toString(),
+                    clienteDAO.lista().get(i).getNomeFantasia(),
+                    clienteDAO.lista().get(i).getCnpjCpf(),
+                    clienteDAO.lista().get(i).getTelefoneList().get(0).getTelefone(),
+                    clienteDAO.lista().get(i).getTelefoneList().get(0).getDescricao()};
+                model.addRow(linha);
+                i++;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+
+    public void selecionarCliente() {
+        if (tb_clientes.getSelectedRowCount() == 0) {
+            JOptionPane.showMessageDialog(null, "Selecione uma linha!");
+        } else {
+            if (tb_clientes.getSelectedRowCount() > 1) {
+                JOptionPane.showMessageDialog(null, "Selecione Apenas uma linha!");
+            } else {
+                Frm_CadUsuario f = new Frm_CadUsuario();
+                f.setCodigoUsuario(Integer.parseInt(tb_clientes.getValueAt(tb_clientes.getSelectedRow(), 0).toString()));
+                f.buscar();
+                dispose();
+            }
+        }
+    }
+
+    public void filtrar(java.awt.event.KeyEvent evt) {
+        TableRowSorter sorter = new TableRowSorter(tb_clientes.getModel());
+        tb_clientes.setRowSorter(sorter);
+        String texto = txt_filtro.getText();
+        try {
+            sorter.setRowFilter(RowFilter.regexFilter("(?i)" + texto));
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Valor Não Encontrado!!!", "AVISO - Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void limpaTabela() {
+        try {
+            while (0<model.getRowCount()) {
+                    model.removeRow(0);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tb_clientes = new javax.swing.JTable();
+        btn_cancelar = new javax.swing.JButton();
+        btn_selecionar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        txt_filtro = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Consulta Clientes");
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        tb_clientes.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Código", "Referência", "Nome Fantasia", "CPF/CNPJ", "Telefone", "Contato"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tb_clientes.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(tb_clientes);
+        if (tb_clientes.getColumnModel().getColumnCount() > 0) {
+            tb_clientes.getColumnModel().getColumn(0).setMinWidth(70);
+            tb_clientes.getColumnModel().getColumn(0).setPreferredWidth(70);
+            tb_clientes.getColumnModel().getColumn(0).setMaxWidth(70);
+            tb_clientes.getColumnModel().getColumn(1).setMinWidth(70);
+            tb_clientes.getColumnModel().getColumn(1).setPreferredWidth(70);
+            tb_clientes.getColumnModel().getColumn(1).setMaxWidth(70);
+            tb_clientes.getColumnModel().getColumn(3).setMinWidth(120);
+            tb_clientes.getColumnModel().getColumn(3).setPreferredWidth(120);
+            tb_clientes.getColumnModel().getColumn(3).setMaxWidth(120);
+            tb_clientes.getColumnModel().getColumn(4).setMinWidth(85);
+            tb_clientes.getColumnModel().getColumn(4).setPreferredWidth(85);
+            tb_clientes.getColumnModel().getColumn(4).setMaxWidth(85);
+            tb_clientes.getColumnModel().getColumn(5).setMinWidth(150);
+            tb_clientes.getColumnModel().getColumn(5).setPreferredWidth(150);
+            tb_clientes.getColumnModel().getColumn(5).setMaxWidth(150);
+        }
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 304, Short.MAX_VALUE)
+        );
+
+        btn_cancelar.setText("Cancelar");
+
+        btn_selecionar.setText("Selecionar");
+
+        jLabel1.setText("Filtro:");
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(btn_cancelar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btn_selecionar, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txt_filtro, javax.swing.GroupLayout.PREFERRED_SIZE, 429, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 190, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(10, 10, 10)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txt_filtro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btn_selecionar)
+                    .addComponent(btn_cancelar))
+                .addContainerGap())
+        );
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Frm_ConAtendimento_Pendentes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Frm_ConAtendimento_Pendentes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Frm_ConAtendimento_Pendentes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Frm_ConAtendimento_Pendentes.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Frm_ConAtendimento_Pendentes().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_cancelar;
+    private javax.swing.JButton btn_selecionar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tb_clientes;
+    private javax.swing.JTextField txt_filtro;
+    // End of variables declaration//GEN-END:variables
+}
