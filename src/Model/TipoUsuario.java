6bea396c7f8cf04f0394888093df89913c4a00a3
Fabyana Diaz <Fabyana Diaz@PC>
2014-11-12 16:35:06 -0200

    Correção Ortográfica - Model

--- src/Model/TipoUsuario.java
+++ src/Model/TipoUsuario.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Model;
 
 import java.io.Serializable;
@@ -22,10 +16,6 @@ import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-/**
- *
- * @author Tadeu
- */
 @Entity
 @Table(name = "tipo_usuario")
 @XmlRootElement
